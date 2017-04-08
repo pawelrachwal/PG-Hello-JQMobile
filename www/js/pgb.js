8a4acbb178942a5d37f56f849264673b6ebb914e
Paweł Rachwał <Pawel@Air-Pawe.home>
2017-04-08 21:22:08 +0200

    Add photos

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -7,14 +7,3 @@ function onDeviceReady() {
 	deviceInfo();
 }
 
-function deviceInfo() {
-
-	info =  'Device Model   : '    + device.model + '<br>' + 
-			'Device Name    : '     + device.name + '<br>' + 
-			'Device Cordova : '  + device.cordova + '<br>' + 
-			'Device Platform: ' + device.platform + '<br>' + 
-			'Device UUID    : '     + device.uuid + '<br>' + 
-			'Device Version : '  + device.version + '<br>';
-
-	document.getElementById("deviceDetails").innerHTML = info;	
-}
